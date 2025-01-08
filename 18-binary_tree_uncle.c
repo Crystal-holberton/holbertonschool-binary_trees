6cbba753c2c2e641cce665a7f576ce21a2dea99f
Crystal-holberton <9188@holbertonstudents.com>
2025-01-08 00:19:58 +0000

    binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,12 +7,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent;
 
 	if (!node || !node->parent || !node->parent->parent)
 	{
 		return (NULL);
 	}
+	grandparent = node->parent->parent;
 	if (grandparent->left == node->parent)
 	{
 		return (grandparent->right);
